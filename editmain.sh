8118e1a5a6291c14732b97b68a0036ca093d371e
nv8h <s2i1x2z0m0@gmail.com>
2014-01-12 04:20:38 -0800

    Modify Shell Script and make it smarter

--- editmain.sh
+++
@@ -1,3 +0,0 @@
-echo "Create Backup..."
-cp "base/script/main.py" "base/script/main.py.bak"
-mcedit "base/script/main.py"
---
+++ ratmain.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+MAINFILE="./base/script/main.py"
+BACKUPFILE="./base/script/main.py.bak"
+
+backup()
+{
+    echo "Create Backup..."
+    cp $MAINFILE $BACKUPFILE
+}
+
+
+case $1 in
+    "") echo "For information type ratmain --help";;
+    "edit")
+        backup()
+        mcedit "base/script/main.py"
+    ;;
+    "diff")
+        diff $MAINFILE $BACKUPFILE
+    ;;
+    "restore")
+        cp $BACKUPFILE $MAINFILE
+    ;;
+    "add")
+        case $2 in
+            "") echo "Usage: ratmain add (mode,def) (name Of mode)";;
+            "mode")
+                backup()
+                echo "    " >> $MAINFILE
+                echo "    def ${3}Action(self):" >> $MAINFILE
+                echo "        print(\"Welcome to the '${3}' mode!\")" >> $MAINFILE
+                echo "        " >> $MAINFILE
+                echo "        # return with Error Message" >> $MAINFILE
+                echo "        return None" >> $MAINFILE
+                echo "${3} mode added to the main.py file"
+            ;;
+            "def")
+                backup()
+                echo "    " >> $MAINFILE
+                echo "    def ${3}(self):" >> $MAINFILE
+                echo "        return None" >> $MAINFILE
+                echo "${3} definition added to the main.py file"
+            ;;
+        esac
+        ;;
+    "--help")
+        echo "ratmain.sh"
+        echo "Writed by Schoffhauzer István"
+        echo "-------------------------------------------------"
+        echo ""
+        echo "Edit main: ratmain.sh edit"
+        echo "Add mode: ratmain.sh add mode name"
+        echo "Add def: ratmain.sh add def name"
+        echo "Restore main: ratmain.sh restore"
+        echo "Diff main: ratmain.sh diff"
+    ;;
+esac
+
+
